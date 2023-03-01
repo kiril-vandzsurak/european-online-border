aee64cc4797f5b6ad71910feb54d0a3c105a7477
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-03-01 16:06:43 +0100

    completing login page for pc

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -4,25 +4,27 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const LoginPage = () => {
   return (
     <div className="backgroundLogin">
       <video src={videoBg} autoPlay loop muted />
-      <div className="content">
+      <div className="contentLogin">
         <Container>
           <Row>
             <Col className="d-flex justify-content-center">
-              <div className="rowDirection">
+              <div className="rowDirectionLogin">
                 <Link to="/" relative="path">
-                  <button className="backButtonRegister">
+                  <button className="backButtonLogin">
                     <img
                       style={{
                         display: "block",
-                        width: "58px",
-                        height: "58px",
+                        width: "17px",
+                        height: "29px",
                       }}
-                      src={window.location.origin + "/left-chevron.png"}
+                      src={window.location.origin + "/left-arrow.png"}
                       alt="img"
                     />
                   </button>
@@ -38,7 +40,7 @@ const LoginPage = () => {
                     alt="img"
                   />
                 </div>
-                <div className="labelNameRegister">EUROPEAN BORDER CONTROL</div>
+                <div className="labelNameLogin">EUROPEAN BORDER CONTROL</div>
                 <div>
                   <img
                     style={{
@@ -54,7 +56,50 @@ const LoginPage = () => {
               </div>
             </Col>
           </Row>
-          <Row></Row>
+          <Row>
+            <Col className="d-flex justify-content-center">
+              <div className="formLogin d-flex flex-column align-items-center">
+                <h1 className="h1Login">Sign in</h1>
+                <Form>
+                  <Form.Group
+                    className="mb-3 d-flex flex-row align-items-center inputMarginsLogin"
+                    controlId="formBasicEmail"
+                  >
+                    <Form.Label className="marginEmailLogin">
+                      Email address
+                    </Form.Label>
+                    <Form.Control
+                      className="inputLogin"
+                      type="email"
+                      placeholder="Enter email"
+                    />
+                  </Form.Group>
+
+                  <Form.Group
+                    className="mb-3 d-flex flex-row align-items-center inputMarginsLogin"
+                    controlId="formBasicPassword"
+                  >
+                    <Form.Label className="marginPasswordLogin">
+                      Password
+                    </Form.Label>
+                    <Form.Control
+                      className="inputLogin"
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </Form.Group>
+                </Form>
+                <Button className="loginBtn" type="submit">
+                  <span>Login</span>
+                </Button>
+                <p className="downTextLogin">
+                  We're committed to your privacy. Our form uses the information
+                  you provide to us to let you access your personal account. You
+                  may log out anytime you want
+                </p>
+              </div>
+            </Col>
+          </Row>
         </Container>
       </div>
     </div>
