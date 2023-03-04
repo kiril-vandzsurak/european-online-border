70442722393b05658251fb296e3b61b6dd39dc72
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-03-04 19:46:01 +0100

    start working on account page
    Creating menu bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage/WelcomePage.jsx";
 import LoginPage from "./components/LoginPage/LoginPage.jsx";
 import RegisterPage from "./components/RegisterPage/RegisterPage.jsx";
+import MyAccount from "./components/MyAccount/MyAccount.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route element={<WelcomePage />} path="/" />
         <Route element={<LoginPage />} path="/login" />
         <Route element={<RegisterPage />} path="/register" />
+        <Route element={<MyAccount />} path="/myAccount" />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/MyAccount/MyAccount.jsx
@@ -0,0 +1,90 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Button } from "react-bootstrap";
+import "./MyAccount.css";
+
+const MyAccount = () => {
+  return (
+    <div>
+      <Container fluid>
+        <Row>
+          <Col lg={2} style={{ padding: "0" }}>
+            <div
+              style={{
+                width: "100%",
+                height: "100vh",
+                backgroundColor: "#f8fff9",
+                borderRadius: "20px",
+              }}
+            >
+              <Button className="menuButtons">
+                <div className="d-flex align-items-center">
+                  <img
+                    style={{
+                      display: "block",
+                      width: "50px",
+                      height: "50px",
+                      marginTop: "3px",
+                    }}
+                    src={window.location.origin + "/travelHistory.png"}
+                    alt="img"
+                  />
+                  <span className="buttonName">New Travel</span>
+                </div>
+              </Button>
+              <Button className="menuButtons">
+                <div className="d-flex align-items-center">
+                  <img
+                    style={{
+                      display: "block",
+                      width: "50px",
+                      height: "50px",
+                      marginTop: "3px",
+                    }}
+                    src={window.location.origin + "/travelHistory.png"}
+                    alt="img"
+                  />
+                  <span className="buttonName">Travel History</span>
+                </div>
+              </Button>
+              <Button className="menuButtons">
+                <div className="d-flex align-items-center">
+                  <img
+                    style={{
+                      display: "block",
+                      width: "50px",
+                      height: "50px",
+                      marginTop: "3px",
+                    }}
+                    src={window.location.origin + "/travelHistory.png"}
+                    alt="img"
+                  />
+                  <span className="buttonName">Information</span>
+                </div>
+              </Button>
+              <Button className="menuButtons">
+                <div className="d-flex align-items-center">
+                  <img
+                    style={{
+                      display: "block",
+                      width: "50px",
+                      height: "50px",
+                      marginTop: "3px",
+                    }}
+                    src={window.location.origin + "/travelHistory.png"}
+                    alt="img"
+                  />
+                  <span className="buttonName">Logout</span>
+                </div>
+              </Button>
+            </div>
+          </Col>
+          <Col lg={10}></Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default MyAccount;
